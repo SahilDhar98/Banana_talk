26acab236fc03040e9307af7cdea8597823a35f7
Sahil Dhar <sahildhar98@gmail.com>
2021-09-29 15:05:45 +0530

    txtinput displayed

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var btntranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input");
 
+console.log(txtinput);
 
 
 btntranslate.addEventListener("click" , function clickeventhandler(){
     console.log("clicked");
+    console.log("input", txtinput.value);
 });
 
+// txtinput.addEventListener("click" , function clickeventhandler(){
+    
+    
+// });
