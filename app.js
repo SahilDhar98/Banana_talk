fae2a1ff38c4685bf64e063134a18ec26e74bd5d
Sahil Dhar <sahildhar98@gmail.com>
2021-09-29 20:32:35 +0530

    output wired

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-var btntranslate = document.querySelector("#btn-translate");
+// button click
+var btntranslate = document.querySelector("#btn-translate"); 
+btntranslate.addEventListener("click" , clickeventhandler);
+// reading Input
 var txtinput = document.querySelector("#txt-input");
-var outputtxt = document.querySelector("#txt-output")
+// output
+var outputtxt = document.querySelector("#txt-output");
 
-var serverURL = "https://api.funtranslations.com/translate/pirate.json"
 
+
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json";
+// Url creator
 function urlcreator(input){
-    return serverURL + "?" + "text=" + input 
+    return serverURL + "?" + "text=" + input ;
 }
 
+function errorHandler(error){
+    alert("error occured", error);
+}
+// Event handler 
+function clickeventhandler(){
+    console.log("Button clicked");
+    var txt = txtinput.value;
+    console.log(txt);
+    console.log(urlcreator(txt));
+    var finalURL = urlcreator(txt);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputtxt.innerText = translatedText; 
+            
+    })
+        .catch(errorHandler)
 
 
-btntranslate.addEventListener("click" , function clickeventhandler(){
-    console.log("clicked");
-    console.log("input", txtinput.value);
-});
-
-// txtinput.addEventListener("click" , function clickeventhandler(){
-    
-    
-// });
+}
+// console.log(outputtxt.innerText);
