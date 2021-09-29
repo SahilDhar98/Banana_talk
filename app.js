32bc85c1f92e7b621b1d7e55fc4faf3a721c9685
Sahil Dhar <sahildhar98@gmail.com>
2021-09-29 20:52:27 +0530

    Minor changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var outputtxt = document.querySelector("#txt-output");
 
 
 
-var serverURL = "https://api.funtranslations.com/translate/pirate.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 // Url creator
 function urlcreator(input){
     return serverURL + "?" + "text=" + input ;
